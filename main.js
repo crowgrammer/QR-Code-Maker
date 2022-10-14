0831f792263f60e340516556e65e7a103f4606ea
Muhammed Aladin <44171554+crowgrammer@users.noreply.github.com>
2022-10-14 16:22:53 +0300

    finished
    all that's left is to add tutorial

--- main.js
+++ main.js
@@ -1,30 +1,61 @@
 import "./style.css";
+
 import readXlsxFile from "read-excel-file";
+import JSZip from "jszip";
+import JSZipUtils from "jszip-utils";
+import { saveAs } from "file-saver";
 
 const genrate = document.getElementById("upload-btn");
 const file = document.getElementById("upload-file");
-const wrapper = document.getElementById("qrs");
 
 let list = [];
 let sources = [];
 file.addEventListener("change", () => {
   readXlsxFile(file.files[0]).then((data) => {
-    //console.table(rows);
-    data.map((row, index) => {
-      list.push(data[(row, index)][0]);
-    });
+    try {
+      data.map((row, index) => {
+        list.push(data[(row, index)][0]);
+      });
+    } catch (error) {
+      alert(error);
+      console.log(error);
+    }
   });
 });
 
 genrate.addEventListener("click", () => {
-  list.map((codeText) => {
-    sources.push(
-      `https://api.qrserver.com/v1/create-qr-code/?size=100x100&data=${codeText}`
-    );
-  });
-  sources.forEach((src) => {
-    let qr = document.createElement("img");
-    qr.src = src;
-    wrapper.appendChild(qr);
-  });
+  try {
+    list.map((codeText) => {
+      sources.push(
+        `https://api.qrserver.com/v1/create-qr-code/?size=100x100&data=${codeText}`
+      );
+    });
+    generateZIP();
+  } catch (err) {
+    console.log(err);
+    alert(err);
+  }
 });
+
+function generateZIP() {
+  var zip = new JSZip();
+  var count = 0;
+  var zipFilename = "Code.zip";
+
+  sources.forEach(function (url, i) {
+    var filename = list[i] + ".png";
+    // loading a file and add it in a zip file
+    JSZipUtils.getBinaryContent(url, function (err, data) {
+      if (err) {
+        throw err; // or handle the error
+      }
+      zip.file(filename, data, { binary: true });
+      count++;
+      if (count == sources.length) {
+        zip.generateAsync({ type: "blob" }).then(function (content) {
+          saveAs(content, zipFilename);
+        });
+      }
+    });
+  });
+}
